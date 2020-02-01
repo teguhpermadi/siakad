afff1eeaf6fe348862e5148875cea795199322a2
teguhpermadi <tguh.permadi@gmail.com>
2020-02-01 11:30:25 +0700

    5.0.0

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ sistem informasi akademik dengan codeigniter
 ============================================
 * 1.0.0 : instal codeigniter
 * 2.0.0 : menghilangkan index.php dan menambahkan file .htaccess
+* 3.0.0 : setting database
+* 4.0.0 : setting autoload
+* 5.0.0 : copy file assets
---
+++ assets/js/custom.js
@@ -0,0 +1,19 @@
+// datatable users
+$(document).ready(function() {
+    $('#datatable-users').DataTable();
+} );
+
+// datatable guru
+$(document).ready(function() {
+    $('#datatable-guru').DataTable();
+} );
+
+// datatable siswa
+$(document).ready(function() {
+    $('#datatable-siswa').DataTable();
+} );
+
+// datatable mapel
+$(document).ready(function() {
+    $('#datatable-mapel').DataTable();
+} );

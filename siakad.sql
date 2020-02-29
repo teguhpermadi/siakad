6579f7cb09f809243e465a7af4f4ec9b177b509e
teguhpermadi <tguh.permadi@gmail.com>
2020-02-29 18:03:04 +0700

    9.13.5

--- siakad.sql
+++ siakad.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.1
+-- version 4.9.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 29, 2020 at 04:28 AM
--- Server version: 10.4.8-MariaDB
--- PHP Version: 7.3.10
+-- Generation Time: Feb 29, 2020 at 11:59 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -28,14 +28,34 @@ SET time_zone = "+00:00";
 -- Table structure for table `absensi`
 --
 
-CREATE TABLE `absensi` (
-  `id` int(255) NOT NULL,
+DROP TABLE IF EXISTS `absensi`;
+CREATE TABLE IF NOT EXISTS `absensi` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
   `id_tahun` int(255) NOT NULL,
   `id_siswa` int(255) NOT NULL,
   `sakit` int(255) NOT NULL,
   `izin` int(255) NOT NULL,
-  `alpa` int(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `alpa` int(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_tahun` (`id_tahun`,`id_siswa`)
+) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `catatan`
+--
+
+DROP TABLE IF EXISTS `catatan`;
+CREATE TABLE IF NOT EXISTS `catatan` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `id_tahun` int(255) NOT NULL,
+  `id_siswa` int(255) NOT NULL,
+  `keterangan` varchar(255) NOT NULL,
+  `note` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_tahun` (`id_tahun`,`id_siswa`)
+) ENGINE=InnoDB AUTO_INCREMENT=81 DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -43,11 +63,13 @@ CREATE TABLE `absensi` (
 -- Table structure for table `groups`
 --
 
-CREATE TABLE `groups` (
-  `id` mediumint(8) UNSIGNED NOT NULL,
+DROP TABLE IF EXISTS `groups`;
+CREATE TABLE IF NOT EXISTS `groups` (
+  `id` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` varchar(20) NOT NULL,
-  `description` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `description` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `groups`
@@ -56,7 +78,7 @@ CREATE TABLE `groups` (
 INSERT INTO `groups` (`id`, `name`, `description`) VALUES
 (1, 'admin', 'Administrator'),
 (2, 'members', 'General User'),
-(3, 'guru', 'Guru'),
+(3, 'guru', 'Guru');
 (4, 'siswa', 'Siswa');
 
 -- --------------------------------------------------------
@@ -65,8 +87,9 @@ INSERT INTO `groups` (`id`, `name`, `description`) VALUES
 -- Table structure for table `guru`
 --
 
-CREATE TABLE `guru` (
-  `id` int(255) NOT NULL,
+DROP TABLE IF EXISTS `guru`;
+CREATE TABLE IF NOT EXISTS `guru` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
   `nama_lengkap` varchar(255) DEFAULT NULL,
   `jenis_kelamin` varchar(255) DEFAULT NULL,
   `tempat_lahir` varchar(255) DEFAULT NULL,
@@ -79,8 +102,9 @@ CREATE TABLE `guru` (
   `gelar_belakang` varchar(255) DEFAULT NULL,
   `pendidikan_terakhir` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
-  `telp` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `telp` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=156 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -88,12 +112,14 @@ CREATE TABLE `guru` (
 -- Table structure for table `kelas`
 --
 
-CREATE TABLE `kelas` (
-  `id` int(100) NOT NULL,
-  `nama` varchar(100) NOT NULL,
+DROP TABLE IF EXISTS `kelas`;
+CREATE TABLE IF NOT EXISTS `kelas` (
+  `id` int(100) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(255) NOT NULL,
+  `kode_kelas` varchar(255) NOT NULL,
   `tingkat` varchar(255) NOT NULL,
-  `kode_kelas` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -101,12 +127,14 @@ CREATE TABLE `kelas` (
 -- Table structure for table `login_attempts`
 --
 
-CREATE TABLE `login_attempts` (
-  `id` int(11) UNSIGNED NOT NULL,
+DROP TABLE IF EXISTS `login_attempts`;
+CREATE TABLE IF NOT EXISTS `login_attempts` (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `ip_address` varchar(45) NOT NULL,
   `login` varchar(100) NOT NULL,
-  `time` int(11) UNSIGNED DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `time` int(11) UNSIGNED DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -114,12 +142,14 @@ CREATE TABLE `login_attempts` (
 -- Table structure for table `mapel`
 --
 
-CREATE TABLE `mapel` (
-  `id` int(255) NOT NULL,
+DROP TABLE IF EXISTS `mapel`;
+CREATE TABLE IF NOT EXISTS `mapel` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
   `nama` varchar(255) DEFAULT NULL,
   `kode` varchar(255) DEFAULT NULL,
-  `kelompok` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `kelompok` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -127,13 +157,15 @@ CREATE TABLE `mapel` (
 -- Table structure for table `pengajar`
 --
 
-CREATE TABLE `pengajar` (
-  `id` int(255) NOT NULL,
+DROP TABLE IF EXISTS `pengajar`;
+CREATE TABLE IF NOT EXISTS `pengajar` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
   `id_guru` int(255) NOT NULL,
   `id_mapel` int(255) NOT NULL,
   `id_kelas` int(255) NOT NULL,
-  `id_tahun` int(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `id_tahun` int(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -141,8 +173,9 @@ CREATE TABLE `pengajar` (
 -- Table structure for table `profil`
 --
 
-CREATE TABLE `profil` (
-  `id` int(255) NOT NULL,
+DROP TABLE IF EXISTS `profil`;
+CREATE TABLE IF NOT EXISTS `profil` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
   `namaSekolah` varchar(255) DEFAULT NULL,
   `npsn` varchar(255) DEFAULT NULL,
   `bentukSekolah` varchar(255) DEFAULT NULL,
@@ -156,7 +189,8 @@ CREATE TABLE `profil` (
   `dusun` varchar(255) DEFAULT NULL,
   `kodePos` varchar(255) DEFAULT NULL,
   `lintang` varchar(255) DEFAULT NULL,
-  `bujur` varchar(255) DEFAULT NULL
+  `bujur` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -165,12 +199,14 @@ CREATE TABLE `profil` (
 -- Table structure for table `rombel`
 --
 
-CREATE TABLE `rombel` (
-  `id` int(100) NOT NULL,
+DROP TABLE IF EXISTS `rombel`;
+CREATE TABLE IF NOT EXISTS `rombel` (
+  `id` int(100) NOT NULL AUTO_INCREMENT,
   `id_tahun` int(255) NOT NULL,
   `id_kelas` int(255) NOT NULL,
-  `id_siswa` int(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `id_siswa` int(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=94 DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -178,8 +214,9 @@ CREATE TABLE `rombel` (
 -- Table structure for table `siswa`
 --
 
-CREATE TABLE `siswa` (
-  `id` int(255) NOT NULL,
+DROP TABLE IF EXISTS `siswa`;
+CREATE TABLE IF NOT EXISTS `siswa` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
   `nama_lengkap` varchar(255) DEFAULT NULL,
   `nama_panggilan` varchar(255) DEFAULT NULL,
   `nis` varchar(255) DEFAULT NULL,
@@ -210,8 +247,9 @@ CREATE TABLE `siswa` (
   `aktif` tinyint(1) DEFAULT NULL,
   `telp_siswa` varchar(255) DEFAULT NULL,
   `telp_ayah` varchar(255) DEFAULT NULL,
-  `telp_ibu` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `telp_ibu` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=167 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -219,13 +257,15 @@ CREATE TABLE `siswa` (
 -- Table structure for table `tahun_pelajaran`
 --
 
-CREATE TABLE `tahun_pelajaran` (
-  `id` int(255) NOT NULL,
+DROP TABLE IF EXISTS `tahun_pelajaran`;
+CREATE TABLE IF NOT EXISTS `tahun_pelajaran` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
   `tahun` varchar(255) DEFAULT NULL,
   `semester` varchar(255) DEFAULT NULL,
   `id_kepsek` int(255) DEFAULT NULL,
-  `tanggal_rapor` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `tanggal_rapor` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -233,8 +273,9 @@ CREATE TABLE `tahun_pelajaran` (
 -- Table structure for table `users`
 --
 
-CREATE TABLE `users` (
-  `id` int(11) UNSIGNED NOT NULL,
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `ip_address` varchar(45) NOT NULL,
   `username` varchar(100) DEFAULT NULL,
   `password` varchar(255) NOT NULL,
@@ -252,16 +293,20 @@ CREATE TABLE `users` (
   `first_name` varchar(50) DEFAULT NULL,
   `last_name` varchar(50) DEFAULT NULL,
   `company` varchar(100) DEFAULT NULL,
-  `phone` varchar(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `phone` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uc_email` (`email`),
+  UNIQUE KEY `uc_activation_selector` (`activation_selector`),
+  UNIQUE KEY `uc_forgotten_password_selector` (`forgotten_password_selector`),
+  UNIQUE KEY `uc_remember_selector` (`remember_selector`)
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `ip_address`, `username`, `password`, `email`, `activation_selector`, `activation_code`, `forgotten_password_selector`, `forgotten_password_code`, `forgotten_password_time`, `remember_selector`, `remember_code`, `created_on`, `last_login`, `active`, `first_name`, `last_name`, `company`, `phone`) VALUES
-(1, '127.0.0.1', 'administrator', '$2y$12$EXgZ3Jar14KxBIdOzZohX.bsAUcSxqUCFijg5G/UMknKa8ndWoHPy', 'admin@admin.com', NULL, '', NULL, NULL, NULL, 'b70fba9e7459695f4ef467bd215edba8c0cd66f8', '$2y$10$Kx9WqnKnBf7/1XACqjQAve/ghv6159rz8/3mFsKQKjFLCfD5kQRHq', 1268889823, 1582935994, 1, 'Admin', 'istrator', 'ADMIN', '0'),
-(29, '::1', 'aricci5@mapquest.com', '$2y$10$9yb/xisNiVh.A/BhF4oyv.ckIE6X91Cmqh96rhL.TB94gidMU1mnK', 'aricci5@mapquest.com', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1582853580, 1582936078, 1, 'Aggie', 'Ricci', NULL, '3291000251');
+(1, '127.0.0.1', 'administrator', '$2y$12$LDGmMnMv/vfhEGd/lVssTuEjTNccQH.QLYYSKHgPg..6sycG9azFu', 'admin@admin.com', NULL, '', NULL, NULL, NULL, NULL, NULL, 1268889823, 1582836496, 1, 'Admin', 'istrator', 'ADMIN', '0'),
 
 -- --------------------------------------------------------
 
@@ -269,11 +314,16 @@ INSERT INTO `users` (`id`, `ip_address`, `username`, `password`, `email`, `activ
 -- Table structure for table `users_groups`
 --
 
-CREATE TABLE `users_groups` (
-  `id` int(11) UNSIGNED NOT NULL,
+DROP TABLE IF EXISTS `users_groups`;
+CREATE TABLE IF NOT EXISTS `users_groups` (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `user_id` int(11) UNSIGNED NOT NULL,
-  `group_id` mediumint(8) UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `group_id` mediumint(8) UNSIGNED NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uc_users_groups` (`user_id`,`group_id`),
+  KEY `fk_users_groups_users1_idx` (`user_id`),
+  KEY `fk_users_groups_groups1_idx` (`group_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `users_groups`
@@ -282,204 +332,20 @@ CREATE TABLE `users_groups` (
 INSERT INTO `users_groups` (`id`, `user_id`, `group_id`) VALUES
 (1, 1, 1),
 (2, 1, 2),
-(10, 29, 3);
-
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `walikelas`
 --
 
-CREATE TABLE `walikelas` (
-  `id` int(255) NOT NULL,
+DROP TABLE IF EXISTS `walikelas`;
+CREATE TABLE IF NOT EXISTS `walikelas` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
   `id_guru` int(255) NOT NULL,
   `id_kelas` int(255) NOT NULL,
-  `id_tahun` int(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `absensi`
---
-ALTER TABLE `absensi`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `id_tahun` (`id_tahun`,`id_siswa`);
-
---
--- Indexes for table `groups`
---
-ALTER TABLE `groups`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `guru`
---
-ALTER TABLE `guru`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `kelas`
---
-ALTER TABLE `kelas`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `login_attempts`
---
-ALTER TABLE `login_attempts`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `mapel`
---
-ALTER TABLE `mapel`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `pengajar`
---
-ALTER TABLE `pengajar`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `profil`
---
-ALTER TABLE `profil`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `rombel`
---
-ALTER TABLE `rombel`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `siswa`
---
-ALTER TABLE `siswa`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `tahun_pelajaran`
---
-ALTER TABLE `tahun_pelajaran`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `uc_email` (`email`),
-  ADD UNIQUE KEY `uc_activation_selector` (`activation_selector`),
-  ADD UNIQUE KEY `uc_forgotten_password_selector` (`forgotten_password_selector`),
-  ADD UNIQUE KEY `uc_remember_selector` (`remember_selector`);
-
---
--- Indexes for table `users_groups`
---
-ALTER TABLE `users_groups`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `uc_users_groups` (`user_id`,`group_id`),
-  ADD KEY `fk_users_groups_users1_idx` (`user_id`),
-  ADD KEY `fk_users_groups_groups1_idx` (`group_id`);
-
---
--- Indexes for table `walikelas`
---
-ALTER TABLE `walikelas`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `absensi`
---
-ALTER TABLE `absensi`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `groups`
---
-ALTER TABLE `groups`
-  MODIFY `id` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT for table `guru`
---
-ALTER TABLE `guru`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `kelas`
---
-ALTER TABLE `kelas`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `login_attempts`
---
-ALTER TABLE `login_attempts`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `mapel`
---
-ALTER TABLE `mapel`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `pengajar`
---
-ALTER TABLE `pengajar`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `profil`
---
-ALTER TABLE `profil`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `rombel`
---
-ALTER TABLE `rombel`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `siswa`
---
-ALTER TABLE `siswa`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `tahun_pelajaran`
---
-ALTER TABLE `tahun_pelajaran`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
-
---
--- AUTO_INCREMENT for table `users_groups`
---
-ALTER TABLE `users_groups`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
-
---
--- AUTO_INCREMENT for table `walikelas`
---
-ALTER TABLE `walikelas`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+  `id_tahun` int(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Constraints for dumped tables

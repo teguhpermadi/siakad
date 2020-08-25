d7a2c89f8815b11198064baea702931fa3b8d589
teguhpermadi <tguh.permadi@gmail.com>
2020-08-26 06:17:52 +0700

    9.14.3

--- application/controllers/Penilaian.php
+++ application/controllers/Penilaian.php
@@ -13,6 +13,7 @@ class Penilaian extends CI_Controller {
         $this->load->model('Penilaian_model');
         $this->load->model('Kelas_model');
         $this->load->model('Rombel_model');
+        $this->load->model('Mapel_model');
         // $this->load->model('Kelas_model');
         // cek user login
         check_login();
@@ -94,11 +95,12 @@ class Penilaian extends CI_Controller {
         $id_guru = user_info()['id_guru'];
 
         $get_kelas = $this->Kelas_model->get_kelas($id_kelas);
+        $get_mapel = $this->Mapel_model->get_mapel($id_mapel);
         $kd_pengetahuan = $this->Penilaian_model->get_kd($id_mapel, $get_kelas['tingkat'], 'pengetahuan');
         $kd_keterampilan = $this->Penilaian_model->get_kd($id_mapel, $get_kelas['tingkat'], 'keterampilan');
         // $siswa = $this->Penilaian_model->get_siswa($id_mapel, $id_kelas, $kd_pengetahuan['id']);
 
-        $filename = '"Penilaian Kelas '.$get_kelas['nama'].' oleh '.user_info()['first_name'].' '.user_info()['last_name'].'.xlsx"';
+        $filename = '"Penilaian '.$get_mapel['nama'].' kelas '.$get_kelas['nama'].' oleh '.user_info()['first_name'].' '.user_info()['last_name'].'.xlsx"';
         
         $helper = new Sample();
         if ($helper->isCli()) {
@@ -119,6 +121,7 @@ class Penilaian extends CI_Controller {
 
         $identitas = [
             ['Penilaian Kelas oleh '.user_info()['first_name'].' '.user_info()['last_name']],
+            ['Mata Pelajaran', $get_mapel['nama']],
             ['Kelas', $get_kelas['nama']],
             ['Tahun Pelajaran', $_SESSION['tahun']],
         ]; 
@@ -156,19 +159,26 @@ class Penilaian extends CI_Controller {
                          //    we want to set these values (default is A1)
         );
 
+        // tulis data identitas
+        $spreadsheet->setActiveSheetIndex(0)
+                ->setCellValue('B1', $id_guru)
+                ->setCellValue('B2', $id_kelas)
+                ->setCellValue('B3', $id_mapel)
+                ->setCellValue('B4', $_SESSION['id_tahun_pelajaran']);
+
         // tulis petunjuk pengisian
         $spreadsheet->setActiveSheetIndex(0)
-                ->setCellValue('F5', 'Petunjuk Pengisian')
-                ->setCellValue('F6', 'Isilah cell warna hijau dan orange dengan interval nilai 0 - 100')
-                ->setCellValue('F7', 'Cell warna hijau mewakili kd pengetahuan')
-                ->setCellValue('F8', 'Cell warna orange mewakili kd keterampilan');
+                ->setCellValue('F6', 'Petunjuk Pengisian')
+                ->setCellValue('F7', 'Isilah cell warna hijau dan orange dengan interval nilai 0 - 100')
+                ->setCellValue('F8', 'Cell warna hijau mewakili kd pengetahuan')
+                ->setCellValue('F9', 'Cell warna orange mewakili kd keterampilan');
 
         // tuliskan array header kolom
         $spreadsheet->getActiveSheet()
         ->fromArray(
             $data_header_siswa, // The data to set
             NULL,        // Array values with this value will not be set
-            'A10'         // Top left coordinate of the worksheet range where
+            'A12'         // Top left coordinate of the worksheet range where
                          //    we want to set these values (default is A1)
         );
 
@@ -177,42 +187,61 @@ class Penilaian extends CI_Controller {
         ->fromArray(
             $data_siswa, // The data to set
             NULL,        // Array values with this value will not be set
-            'B11'         // Top left coordinate of the worksheet range where
+            'B13'         // Top left coordinate of the worksheet range where
                          //    we want to set these values (default is A1)
         );
 
         // tulis nomor urut
         for ($i=0; $i < count($data_siswa); $i++) { 
-            $row = 11 + $i;
+            $row = 13 + $i;
             $no = 1 + $i;
             $spreadsheet->setActiveSheetIndex(0)
                 ->setCellValue('A'.$row, $no);
         }
 
+        // ATUR KOLOM DI EXCEL
         // jadikan patokan column
-        $column = range('J', 'Z');
+        $column_asli = range('A', 'Z');
+        $column_tambahan = [];
+
+        // tambahkan kolom berikutnya
+        foreach($column_asli as $c){
+            array_push($column_tambahan, 'A'.$c);
+        }
+
+        foreach($column_asli as $c){
+            array_push($column_tambahan, 'B'.$c);
+        }
+
+        foreach($column_asli as $c){
+            array_push($column_tambahan, 'C'.$c);
+        }
+
+        // gabungkan semua kolom
+        $column_all = array_merge($column_asli, $column_tambahan);
+        
         // tuliskan array kd dan nilai pada masing-masing kd
         for ($i=0; $i < count($data_kd); $i++) { 
-
+            $i_column = 10 + $i;
             // tulis id kdnya pada baris ke 9
             $spreadsheet->setActiveSheetIndex(0)
-                ->setCellValue($column[$i].'9', $data_kd[$i]['id_kd']);
+                ->setCellValue($column_all[$i_column].'11', $data_kd[$i]['id_kd']);
 
             // tulis header kdnya pada baris ke 10
             $spreadsheet->setActiveSheetIndex(0)
-                ->setCellValue($column[$i].'10', $data_kd[$i]['kd']);
+                ->setCellValue($column_all[$i_column].'12', $data_kd[$i]['kd']);
             
             $data_nilai = $this->Penilaian_model->get_siswa($id_mapel, $id_kelas, $data_kd[$i]['id_kd']);
 
             // beri warna pada cell
             if($data_kd[$i]['jenis'] == 'pengetahuan')
                 {
-                    $spreadsheet->getActiveSheet()->getStyle($column[$i].'10')
+                    $spreadsheet->getActiveSheet()->getStyle($column_all[$i_column].'12')
                     ->getFill()
                     ->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)
                     ->getStartColor()->setARGB('80f700');
                 } else {
-                    $spreadsheet->getActiveSheet()->getStyle($column[$i].'10')
+                    $spreadsheet->getActiveSheet()->getStyle($column_all[$i_column].'12')
                     ->getFill()
                     ->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)
                     ->getStartColor()->setARGB('ff6500');
@@ -223,15 +252,15 @@ class Penilaian extends CI_Controller {
                 $nilai = $data_nilai[$n]['nilai'];
                 
                 // ditulis pada baris ke 6 sampai seterusnya
-                $row = $n + 11;
+                $row = $n + 13;
 
                 // tuliskan masing-masing nilai per kd nya
                 $spreadsheet->setActiveSheetIndex(0)
-                    ->setCellValue($column[$i].$row, $nilai);
+                    ->setCellValue($column_all[$i_column].$row, $nilai);
 
                 // tidak di proteksi
                 $spreadsheet->getActiveSheet()
-                    ->getStyle($column[$i].$row) // proteksi cell
+                    ->getStyle($column_all[$i_column].$row) // proteksi cell
                     ->getProtection()->setLocked(
                         Protection::PROTECTION_UNPROTECTED
                     );
@@ -239,18 +268,26 @@ class Penilaian extends CI_Controller {
                 // beri warna pada cell
                 if($data_kd[$i]['jenis'] == 'pengetahuan')
                 {
-                    $spreadsheet->getActiveSheet()->getStyle($column[$i].$row)
+                    $spreadsheet->getActiveSheet()->getStyle($column_all[$i_column].$row)
                     ->getFill()
                     ->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)
                     ->getStartColor()->setARGB('80f700');
                 } else {
-                    $spreadsheet->getActiveSheet()->getStyle($column[$i].$row)
+                    $spreadsheet->getActiveSheet()->getStyle($column_all[$i_column].$row)
                     ->getFill()
                     ->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)
                     ->getStartColor()->setARGB('ff6500');
             };
             }
         }
+
+        // hide column B,C,D,E
+        $spreadsheet->getActiveSheet()->getColumnDimension('B')->setVisible(false);
+        $spreadsheet->getActiveSheet()->getColumnDimension('C')->setVisible(false);
+        $spreadsheet->getActiveSheet()->getColumnDimension('D')->setVisible(false);
+        $spreadsheet->getActiveSheet()->getColumnDimension('E')->setVisible(false);
+        // hide row 11
+        $spreadsheet->getActiveSheet()->getRowDimension('11')->setVisible(false);
 
 
         // exit;
@@ -265,7 +302,7 @@ class Penilaian extends CI_Controller {
         //     );
 
         // Rename worksheet
-        $spreadsheet->getActiveSheet()->setTitle('Nilai Sikap');
+        $spreadsheet->getActiveSheet()->setTitle('Nilai Mapel');
 
         // Set active sheet index to the first sheet, so Excel opens this as the first sheet
         $spreadsheet->setActiveSheetIndex(0);

a762b2ab7aae5c94d8406072c1259884748383ea
teguhpermadi <tguh.permadi@gmail.com>
2020-09-03 21:14:26 +0700

    9.14.9

--- application/views/nilai/do_nilai.php
+++ application/views/nilai/do_nilai.php
@@ -210,6 +210,45 @@
 
 <script src="<?= base_url('assets/vendor/chart.js/Chart.js'); ?>"></script>
 <script src="<?= base_url()?>assets/vendor/jquery/jquery.js"></script>
+
+<script>
+ 
+  //Create horizontalBar plug-in for ChartJS
+  var originalLineDraw = Chart.controllers.horizontalBar.prototype.draw;
+  Chart.helpers.extend(Chart.controllers.horizontalBar.prototype, {
+  
+      draw: function () {
+          originalLineDraw.apply(this, arguments);
+  
+          var chart = this.chart;
+          var ctx = chart.chart.ctx;
+  
+          var index = chart.config.options.lineAtIndex;
+          if (index) {
+  
+              var xaxis = chart.scales['x-axis-0'];
+              var yaxis = chart.scales['y-axis-0'];
+  
+              var x1 = xaxis.getPixelForValue(index);                       
+              var y1 = 0;                                                   
+  
+              var x2 = xaxis.getPixelForValue(index);                       
+              var y2 = yaxis.height;                                        
+  
+              ctx.save();
+              ctx.beginPath();
+              ctx.moveTo(x1, y1);
+              ctx.strokeStyle = 'red';
+              ctx.lineTo(x2, y2);
+              ctx.stroke();
+  
+              ctx.restore();
+          }
+      }
+  });
+
+</script>
+
 <script>
 function get_chart() {
 			$.get('<?= base_url("penilaian/get_avg/".$id_mapel.'-'.$id_kelas); ?>')
@@ -229,7 +268,8 @@ function get_chart() {
 								"data": rerata,
 								"fill": false,
 								"backgroundColor": colors,
-							}]
+							}
+							]
 						},
 						"options": {
 							"scales": {
@@ -240,6 +280,7 @@ function get_chart() {
 									}
 								}],
 							},
+							"lineAtIndex": 75,
 							"legend": {
 								"display": false
 							},
